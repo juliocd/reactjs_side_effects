3af668b1d11ed1a0a30227b5dff94b1df2e1ab63
Julio Diaz <>
2021-05-15 11:51:48 -0500

    Avoid multiple calls using Effect hook

--- src/components/Login/Login.js
+++ src/components/Login/Login.js
@@ -12,10 +12,21 @@ const Login = (props) => {
   const [formIsValid, setFormIsValid] = useState(false);
 
   useEffect(()=>{
-    setFormIsValid(
-      enteredEmail.includes('@') && enteredPassword.trim().length > 6
-    );
-  }, [enteredEmail, enteredPassword]) // React render if one of the dependencies changes
+    // Avoid multiple calls
+    const identifier = setTimeout(() => {
+      console.log('Checking from validity');
+      setFormIsValid(
+        enteredEmail.includes('@') && enteredPassword.trim().length > 6
+      );
+    }, 500);
+
+    // This function executes after the first component changes
+    return () => {
+      console.log('CLEANUP');
+      clearTimeout(identifier);
+    }
+
+  }, [enteredEmail, enteredPassword])
 
   const emailChangeHandler = (event) => {
     setEnteredEmail(event.target.value);
